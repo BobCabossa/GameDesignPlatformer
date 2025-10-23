80b83112df416ab55c28c149d73f0e44c29d85af
Kasper K <Kasperklassen@gmail.com>
2025-10-23 14:31:46 +0200

    Now there are a simple win

--- Escaping Home/Assets/Scripts/Player/PlayerCollider.cs
+++ Escaping Home/Assets/Scripts/Player/PlayerCollider.cs
@@ -7,6 +7,12 @@ public class PlayerCollider : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("DeathZone"))
+        {
             Player.Die();
+        }
+        else if (collision.CompareTag("WinZone"))
+        {
+            Debug.Log("Player won this level!");
+        }
     }
 }
