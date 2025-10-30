94856f6cb3c87387ffee7270a99583b093658013
Kasper K <Kasperklassen@gmail.com>
2025-10-30 09:17:43 +0100

    Player can't get stuck on the wall

--- Escaping Home/Assets/Scripts/Player/PlayerMovement.cs
+++ Escaping Home/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,7 +7,22 @@ public class PlayerMovement : MonoBehaviour
     [Space(10), SerializeField]
     private Player Player;
 
+    [SerializeField, Space(10)]
+    private Transform rayOrigin;
+
+    [SerializeField]
+    private float rayDistance = 0.1f;
+
+    [SerializeField]
+    private float rayVerticalSpacing = 0.5f;
+
     private float moveInput;
+    private int wallLayerMask;
+
+    private void Start()
+    {
+        wallLayerMask = LayerMask.GetMask("Ground");
+    }
 
     public void SetupControllers()
     {
@@ -39,6 +54,36 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Player.Rigidbody.linearVelocityX = moveInput * Player.MoveSpeed;
+        float move = moveInput * Player.MoveSpeed;
+        Player.Rigidbody.linearVelocityX = AllowPlayerToMove(move) ? move : 0;
+    }
+
+    private bool AllowPlayerToMove(float move)
+    {
+        Vector2 direction = new(Mathf.Sign(move), 0);
+        Vector2[] rayStartOffsets = new Vector2[]
+        {
+            new(0,  rayVerticalSpacing),// Top
+            Vector2.zero,               // Middle
+            new(0, -rayVerticalSpacing) // Bottom
+        };
+
+        foreach (var offset in rayStartOffsets)
+        {
+            Vector2 origin = (Vector2)rayOrigin.position + offset;
+            RaycastHit2D hit = Physics2D.Raycast(origin, direction, rayDistance, wallLayerMask);
+
+            if (hit.collider != null)
+            {
+                Debug.DrawRay(origin, direction * hit.distance, Color.red);
+                return false;
+            }
+            else
+            {
+                Debug.DrawRay(origin, direction * rayDistance, Color.green);
+            }
+        }
+
+        return true;
     }
 }
